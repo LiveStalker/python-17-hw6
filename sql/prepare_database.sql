6e56d2899f6b2b34816edf955b8de7769a71e442
Alexey V. Grebenshchikov <mi.aleksio@gmail.com>
2017-08-14 11:38:32 +0300

    Initial commit

---
+++ sql/prepare_database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE hasker;
+CREATE USER hasker WITH PASSWORD '';
+
+ALTER ROLE hasker SET client_encoding TO 'utf8';
+ALTER ROLE hasker SET default_transaction_isolation TO 'read committed';
+ALTER ROLE hasker SET timezone TO 'UTC';
+
+GRANT ALL PRIVILEGES ON DATABASE hasker TO hasker;
